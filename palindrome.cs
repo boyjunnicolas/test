a8a60a019c3ad33e80941c5dea6049cdcd52e13a
Boyjun <bnicolas0629@gmail.com>
2019-05-08 14:49:35 +0800

    uncomment palidrome code

--- palindrome.cs
+++ palindrome.cs
@@ -1,57 +1,57 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace practice
-//{
-//    class palindrome
-//    {
-//        string _inputString = "";
+namespace practice
+{
+    class palindrome
+    {
+        string _inputString = "";
 
-//        Console.WriteLine("Please enter word");
-//            _inputString = Console.ReadLine();
+        Console.WriteLine("Please enter word");
+            _inputString = Console.ReadLine();
 
-//            string reverseWord = "";
-//        //for (int i = _inputString.Length - 1; i >= 0; i--)
-//        //{
-//        //    reverseWord += _inputString[i];
-//        //}
+            string reverseWord = "";
+        //for (int i = _inputString.Length - 1; i >= 0; i--)
+        //{
+        //    reverseWord += _inputString[i];
+        //}
 
 
-//        //if (reverseWord == _inputString)
-//        //{
-//        //    Console.WriteLine("This is a palindrome");
-//        //}
-//        //else
-//        //{
-//        //    Console.WriteLine("This is not a palindrome");
-//        //}
-//        //Console.ReadLine();
-//        string newString = "";
-//        char[] _array = _inputString.ToCharArray();
-//        _array = _array.Reverse().ToArray();
+        //if (reverseWord == _inputString)
+        //{
+        //    Console.WriteLine("This is a palindrome");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("This is not a palindrome");
+        //}
+        //Console.ReadLine();
+        string newString = "";
+        char[] _array = _inputString.ToCharArray();
+        _array = _array.Reverse().ToArray();
 
-//            foreach (char x  in _array)
-//            {
-//                newString += x;
-//            }
+            foreach (char x  in _array)
+            {
+                newString += x;
+            }
             
 
-//            if (_inputString == newString)
-//            {
-//                Console.WriteLine("This is a palindrome");
-//                Console.WriteLine(newString);
-//                Console.WriteLine(_inputString);
-//            }
-//            else
-//            {
-//                Console.WriteLine("This is not a palindrome");
-//                Console.WriteLine(newString);
-//                Console.WriteLine(_inputString);
-//            }
-//            Console.ReadLine();
+            if (_inputString == newString)
+            {
+                Console.WriteLine("This is a palindrome");
+                Console.WriteLine(newString);
+                Console.WriteLine(_inputString);
+            }
+            else
+            {
+                Console.WriteLine("This is not a palindrome");
+                Console.WriteLine(newString);
+                Console.WriteLine(_inputString);
+            }
+            Console.ReadLine();
     
-//    }
-//}
+    }
+}
